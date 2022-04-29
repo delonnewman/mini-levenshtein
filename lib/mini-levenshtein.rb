7cb5d660a162360aec452ef4540b5ef6d0ea105f
Delon R. Newman <contact@delonnewman.name>
2022-04-29 16:35:44 -0500

    Add spec and validate types on functions

--- lib/mini-levenshtein.rb
+++ lib/mini-levenshtein.rb
@@ -3,17 +3,31 @@ require 'mini-levenshtein/mini_levenshtein'
 module MiniLevenshtein
   extend MiniLevenshteinInternal
 
-  def self.distance(string1, string2)
-    internal_distance(string1, string2, 0)
-  end
+  class << self
+    def distance(string1, string2)
+      validate_string!(string1)
+      validate_string!(string2)
+      
+      internal_distance(string1, string2, 0)
+    end
+  
+    def ratio(string1, string2)
+      validate_string!(string1)
+      validate_string!(string2)
 
-  def self.ratio(string1, string2)
-    lensum = string1.length + string2.length
-    return 1.0 if lensum.zero?
+      lensum = string1.length + string2.length
+      return 1.0 if lensum.zero?
+  
+      distance = internal_distance(string1, string2, 1)
+      return 1.0 if distance.zero?
+  
+      (lensum - distance) / lensum.to_f
+    end
 
-    distance = internal_distance(string1, string2, 1)
-    return 1.0 if distance.zero?
+    private
 
-    (lensum - distance) / lensum.to_f
+    def validate_string!(string)
+      raise TypeError, "no implicit conversion of #{string.class} to String" unless string.is_a?(String)
+    end
   end
 end
---
+++ spec/mini_levenshtein_spec.rb
@@ -0,0 +1,76 @@
+RSpec.describe MiniLevenshtein do
+  describe '.distance' do
+    context 'when either argument is not a string' do
+      it 'will raise a type error' do
+        pairs_of_string_and_non_string.each do |(a, b)|
+          expect { described_class.distance(a, b) }.to raise_error(TypeError)
+        end
+      end
+    end
+
+    context 'when both arguments are strings' do
+      it 'will return an edit distance' do
+        examples = 50.times.map { 2.times.map { Faker::String.random } }
+
+        examples.each do |(a, b)|
+          expect(described_class.distance(a, b)).to be_a(Numeric)
+        end
+      end
+    end
+
+    context 'when both arguments are strings with the same content' do
+      it 'will return 0' do
+        examples = 50.times.map { s = Faker::String.random; [s, s.dup] }
+
+        examples.each do |(a, b)|
+          expect(described_class.distance(a, b)).to be_zero
+        end
+      end
+    end
+  end
+
+  describe '.ratio' do
+    context 'when either argument is not a string' do
+      it 'will raise a type error' do
+        pairs_of_string_and_non_string.each do |(a, b)|
+          expect { described_class.ratio(a, b) }.to raise_error(TypeError)
+        end
+      end
+    end
+
+    context 'when both arguments are empty strings' do
+      it 'will return 1.0' do
+        expect(described_class.ratio('', '')).to be 1.0
+      end
+    end
+
+    context 'when both arguments are strings with the same content' do
+      it 'will return 1.0' do
+        examples = 50.times.map { s = Faker::String.random; [s, s.dup] }
+
+        examples.each do |(a, b)|
+          expect(described_class.ratio(a, b)).to be 1.0
+        end
+      end
+    end
+
+    context 'when both arguments are strings' do
+      it 'will return a Float ratio' do
+        examples = 50.times.map { 2.times.map { Faker::String.random } }
+
+        examples.each do |(a, b)|
+          expect(described_class.ratio(a, b)).to be_a(Float)
+        end
+      end
+    end
+  end
+
+  def pairs_of_string_and_non_string
+    [nil, 1, true, false, [], {}, :testing].each_with_index.map do |x, i|
+      [].tap do |a|
+        a[i % 2] = x
+        a[(i % 2) ^ 1] = ''
+      end
+    end
+  end
+end
