506caeabebf536b78cc181d9b30949c5cc5a87f5
Delon R. Newman <contact@delonnewman.name>
2022-04-29 14:44:20 -0500

    Add levenshtein.c and levenshtein.h

--- ext/mini_levenshtein/extconf.rb
+++ ext/mini_levenshtein/extconf.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 # Loads mkmf which is used to make makefiles for Ruby extensions
-require "mkmf"
+require 'mkmf'
 
 # The destination
-dir_config("mini-levenshtein/mini_levenshtein")
+dir_config('mini-levenshtein/mini_levenshtein')
 
 # Do the work
-create_makefile("mini-levenshtein/mini_levenshtein")
+create_makefile('mini-levenshtein/mini_levenshtein')
--- ext/mini_levenshtein/mini_levenshtein.c
+++ ext/mini_levenshtein/mini_levenshtein.c
@@ -2,202 +2,31 @@
 #include <math.h>
 #include <wchar.h>
 #include <stdlib.h>
-#include <stdio.h>
+
 #include "ruby.h"
-
-static void *
-safe_malloc(size_t nmemb, size_t size) {
-  /* extra-conservative overflow check */
-  if (SIZE_MAX / size <= nmemb) {
-    return NULL;
-  }
-  return malloc(nmemb * size);
-}
-
-/**
- * lev_u_edit_distance:
- * @len1: The length of @string1.
- * @string1: A sequence of Unicode characters of length @len1, may contain NUL
- *           characters.
- * @len2: The length of @string2.
- * @string2: A sequence of Unicode characters of length @len2, may contain NUL
- *           characters.
- * @xcost: If nonzero, the replace operation has weight 2, otherwise all
- *         edit operations have equal weights of 1.
- *
- * Computes Levenshtein edit distance of two Unicode strings.
- *
- * Returns: The edit distance.
- **/
-size_t
-lev_u_edit_distance(size_t len1, const wchar_t *string1,
-                    size_t len2, const wchar_t *string2,
-                    int xcost)
-{
-  size_t i;
-  size_t *row;  /* we only need to keep one row of costs */
-  size_t *end;
-  size_t half;
-
-  /* strip common prefix */
-  while (len1 > 0 && len2 > 0 && *string1 == *string2) {
-    len1--;
-    len2--;
-    string1++;
-    string2++;
-  }
-
-  /* strip common suffix */
-  while (len1 > 0 && len2 > 0 && string1[len1-1] == string2[len2-1]) {
-    len1--;
-    len2--;
-  }
-
-  /* catch trivial cases */
-  if (len1 == 0)
-    return len2;
-  if (len2 == 0)
-    return len1;
-
-  /* make the inner cycle (i.e. string2) the longer one */
-  if (len1 > len2) {
-    size_t nx = len1;
-    const wchar_t *sx = string1;
-    len1 = len2;
-    len2 = nx;
-    string1 = string2;
-    string2 = sx;
-  }
-  /* check len1 == 1 separately */
-  if (len1 == 1) {
-    wchar_t z = *string1;
-    const wchar_t *p = string2;
-    for (i = len2; i; i--) {
-      if (*(p++) == z)
-        return len2 - 1;
-    }
-    return len2 + (xcost != 0);
-  }
-  len1++;
-  len2++;
-  half = len1 >> 1;
-
-  /* initalize first row */
-  row = (size_t*)safe_malloc(len2, sizeof(size_t));
-  if (!row)
-    return (size_t)(-1);
-  end = row + len2 - 1;
-  for (i = 0; i < len2 - (xcost ? 0 : half); i++)
-    row[i] = i;
-
-  /* go through the matrix and compute the costs.  yes, this is an extremely
-   * obfuscated version, but also extremely memory-conservative and relatively
-   * fast.  */
-  if (xcost) {
-    for (i = 1; i < len1; i++) {
-      size_t *p = row + 1;
-      const wchar_t char1 = string1[i - 1];
-      const wchar_t *char2p = string2;
-      size_t D = i - 1;
-      size_t x = i;
-      while (p <= end) {
-        if (char1 == *(char2p++))
-          x = D;
-        else
-          x++;
-        D = *p;
-        if (x > D + 1)
-          x = D + 1;
-        *(p++) = x;
-      }
-    }
-  }
-  else {
-    /* in this case we don't have to scan two corner triangles (of size len1/2)
-     * in the matrix because no best path can go throught them. note this
-     * breaks when len1 == len2 == 2 so the memchr() special case above is
-     * necessary */
-    row[0] = len1 - half - 1;
-    for (i = 1; i < len1; i++) {
-      size_t *p;
-      const wchar_t char1 = string1[i - 1];
-      const wchar_t *char2p;
-      size_t D, x;
-      /* skip the upper triangle */
-      if (i >= len1 - half) {
-        size_t offset = i - (len1 - half);
-        size_t c3;
-
-        char2p = string2 + offset;
-        p = row + offset;
-        c3 = *(p++) + (char1 != *(char2p++));
-        x = *p;
-        x++;
-        D = x;
-        if (x > c3)
-          x = c3;
-        *(p++) = x;
-      }
-      else {
-        p = row + 1;
-        char2p = string2;
-        D = x = i;
-      }
-      /* skip the lower triangle */
-      if (i <= half + 1)
-        end = row + len2 + i - half - 2;
-      /* main */
-      while (p <= end) {
-        size_t c3 = --D + (char1 != *(char2p++));
-        x++;
-        if (x > c3)
-          x = c3;
-        D = *p;
-        D++;
-        if (x > D)
-          x = D;
-        *(p++) = x;
-      }
-      /* lower triangle sentinel */
-      if (i <= half) {
-        size_t c3 = --D + (char1 != *char2p);
-        x++;
-        if (x > c3)
-          x = c3;
-        *p = x;
-      }
-    }
-  }
-
-  i = *end;
-  free(row);
-  return i;
-}
+#include "levenshtein.h"
 
 VALUE MiniLevenshteinInternal = Qnil;
 
 VALUE method_internal_distance(VALUE self, VALUE s1, VALUE s2, VALUE xcost);
 
-void Init_mini_levenshtein() {
+void Init_mini_levenshtein()
+{
   MiniLevenshteinInternal = rb_define_module("MiniLevenshteinInternal");
   rb_define_method(MiniLevenshteinInternal, "internal_distance", method_internal_distance, 3);
 }
 
-VALUE method_internal_distance(VALUE self, VALUE s1, VALUE s2, VALUE xcost) {
+VALUE method_internal_distance(VALUE self, VALUE s1, VALUE s2, VALUE xcost)
+{
   size_t len1 = RSTRING_LEN(s1);
   size_t len2 = RSTRING_LEN(s2);
-  
-  const char *chars1 = StringValuePtr(s1);
-  wchar_t *str1 = (wchar_t *)malloc(sizeof(wchar_t));
-  mbstowcs(str1, chars1, len1);
-  
-  const char *chars2 = StringValuePtr(s2);
-  wchar_t *str2 = (wchar_t *)malloc(sizeof(wchar_t));
-  mbstowcs(str2, chars2, len2);
-  
+
+  const lev_byte *str1 = StringValuePtr(s1);
+  const lev_byte *str2 = StringValuePtr(s2);
+
   int cost = FIX2INT(xcost);
 
-  size_t distance = lev_u_edit_distance(len1, str1, len2, str2, cost);
+  long distance = lev_edit_distance(len1, str1, len2, str2, cost);
 
   return INT2NUM(distance);
 }
