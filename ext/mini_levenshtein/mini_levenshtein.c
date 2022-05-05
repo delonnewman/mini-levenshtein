3f03d2f2aa6a7de04ae710fcf1ba3927df081620
Delon R. Newman <contact@delonnewman.name>
2022-05-04 18:35:10 -0600

    Add documentation to C code

--- ext/mini_levenshtein/mini_levenshtein.c
+++ ext/mini_levenshtein/mini_levenshtein.c
@@ -9,14 +9,6 @@ VALUE rb_lev_edit_distance(VALUE self, VALUE s1, VALUE s2, VALUE xcost);
 VALUE rb_edit_distance(VALUE self, VALUE s1, VALUE s2);
 VALUE rb_similarity(VALUE self, VALUE s1, VALUE s2);
 
-void Init_mini_levenshtein()
-{
-  MiniLevenshtein = rb_define_module("MiniLevenshtein");
-  rb_define_singleton_method(MiniLevenshtein, "lev_edit_distance", rb_lev_edit_distance, 3);
-  rb_define_singleton_method(MiniLevenshtein, "edit_distance", rb_edit_distance, 2);
-  rb_define_singleton_method(MiniLevenshtein, "similarity", rb_similarity, 2);
-}
-
 VALUE rb_lev_edit_distance(VALUE self, VALUE s1, VALUE s2, VALUE xcost)
 {
   Check_Type(s1, T_STRING);
@@ -36,32 +28,63 @@ VALUE rb_lev_edit_distance(VALUE self, VALUE s1, VALUE s2, VALUE xcost)
   return INT2NUM(distance);
 }
 
-VALUE rb_edit_distance(VALUE self, VALUE s1, VALUE s2)
+/*
+ * Compute absolute Levenshtein distance of two strings.
+ *
+ * @example it's hard to spell Levenshtein correctly
+ *   edit_distance('Levenshtein', 'Lenvinsten')  # => 4
+ *   edit_distance('Levenshtein', 'Levensthein') # => 2
+ *   edit_distance('Levenshtein', 'Levenshten')  # => 1
+ *   edit_distance('Levenshtein', 'Levenshtein') # => 0
+ *   "Yeah, we've managed it at last."
+ *
+ * @param string1 [String]
+ * @param string2 [String]
+ *
+ * @return [Integer]
+ */
+VALUE rb_edit_distance(VALUE self, VALUE string1, VALUE string2)
 {
-  Check_Type(s1, T_STRING);
-  Check_Type(s2, T_STRING);
+  Check_Type(string1, T_STRING);
+  Check_Type(string2, T_STRING);
   
-  size_t len1 = RSTRING_LEN(s1);
-  size_t len2 = RSTRING_LEN(s2);
+  size_t len1 = RSTRING_LEN(string1);
+  size_t len2 = RSTRING_LEN(string2);
 
-  const lev_byte *str1 = StringValuePtr(s1);
-  const lev_byte *str2 = StringValuePtr(s2);
+  const lev_byte *str1 = StringValuePtr(string1);
+  const lev_byte *str2 = StringValuePtr(string2);
 
   long distance = lev_edit_distance(len1, str1, len2, str2, 0);
 
   return INT2NUM(distance);
 }
 
-VALUE rb_similarity(VALUE self, VALUE s1, VALUE s2)
+/*
+ * Compute similarity of two strings.
+ *
+ * The similarity is a number between 0 and 1.
+ *
+ * @example
+ *   similarity('Hello World!', 'Holly grail!') # => 0.5833...
+ *
+ * @example
+ *   similarity('Holmes', 'Jack') # => 0.0
+ *
+ * @param string1 [String]
+ * @param string2 [String]
+ *
+ * @return [Float]
+ */
+VALUE rb_similarity(VALUE self, VALUE string1, VALUE string2)
 {
-  Check_Type(s1, T_STRING);
-  Check_Type(s2, T_STRING);
+  Check_Type(string1, T_STRING);
+  Check_Type(string2, T_STRING);
   
-  size_t len1 = RSTRING_LEN(s1);
-  size_t len2 = RSTRING_LEN(s2);
+  size_t len1 = RSTRING_LEN(string1);
+  size_t len2 = RSTRING_LEN(string2);
 
-  const lev_byte *str1 = StringValuePtr(s1);
-  const lev_byte *str2 = StringValuePtr(s2);
+  const lev_byte *str1 = StringValuePtr(string1);
+  const lev_byte *str2 = StringValuePtr(string2);
 
   size_t lensum = len1 + len2;
   if (lensum == 0) {
@@ -75,3 +98,12 @@ VALUE rb_similarity(VALUE self, VALUE s1, VALUE s2)
 
   return DBL2NUM((lensum - distance) / (double)lensum);
 }
+
+void Init_mini_levenshtein()
+{
+  MiniLevenshtein = rb_define_module("MiniLevenshtein");
+  rb_define_singleton_method(MiniLevenshtein, "lev_edit_distance", rb_lev_edit_distance, 3);
+  rb_define_singleton_method(MiniLevenshtein, "edit_distance", rb_edit_distance, 2);
+  rb_define_singleton_method(MiniLevenshtein, "similarity", rb_similarity, 2);
+}
+
--- lib/mini-levenshtein.rb
+++ lib/mini-levenshtein.rb
@@ -2,35 +2,7 @@ require 'mini-levenshtein/mini_levenshtein'
 
 module MiniLevenshtein
   class << self
-    # Compute absolute Levenshtein distance of two strings.
-    #
-    # @example (it's hard to spell Levenshtein correctly):
-    #   distance('Levenshtein', 'Lenvinsten')  # => 4
-    #   distance('Levenshtein', 'Levensthein') # => 2
-    #   distance('Levenshtein', 'Levenshten')  # => 1
-    #   distance('Levenshtein', 'Levenshtein') # => 0
-    #   "Yeah, we've managed it at last."
-    #
-    # @param string1 [String]
-    # @param string2 [String]
-    #
-    # @return [Integer]
     alias distance edit_distance
-
-    # Compute similarity of two strings.
-    #
-    # The similarity is a number between 0 and 1.
-    #
-    # @example
-    #   similarity('Hello World!', 'Holly grail!') # => 0.5833...
-    #
-    # @example
-    #   similarity('Holmes', 'Jack') # => 0.0
-    #
-    # @param string1 [String]
-    # @param string2 [String]
-    #
-    # @return [Float]
     alias ratio similarity
   end
 end
